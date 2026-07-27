bdcfe07cffca2ff6ea75a5d809bf8c74d2eb025c
Tri250 <Tri250@users.noreply.github.com>
2026-07-27 05:10:29 +0000

    chore: bump version to 2.1.3

--- PoseAI-Android/app/build.gradle.kts
+++ PoseAI-Android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.poseai.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.1.2"
+        versionCode = 6
+        versionName = "2.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
